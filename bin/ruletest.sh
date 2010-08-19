9cb7945b9015a883132d60dd92f49b7d1290b62d
ddp <ddpbsd@gmail.com>
2010-08-19 12:38:12 -0400

    Add argus.

--- bin/ruletest.sh
+++ bin/ruletest.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 HOMEDIR=/home/ddp/src/projects/hg/wip-ossec-rules
-RULES="arpwatch avahi bsd_kernel bsd clamav cvsyncd dbus ddclient dropbear flowd hp identd irqbalance isakmpd linux_kernel mountd named ntpd pam pulseaudio python samba sensorsd smartd smtpd snmpd sshd su syslogd"
+RULES="argus arpwatch avahi bsd_kernel bsd clamav cvsyncd dbus ddclient dropbear flowd hp identd irqbalance isakmpd linux_kernel mountd named ntpd pam pulseaudio python samba sensorsd smartd smtpd snmpd sshd su syslogd"
 MAINOUT=`mktemp /tmp/wip-ossec-rules.XXXXXX` || ( echo "Cannot create tempfile!" ; exit 1 )
 
 cd ${HOMEDIR}/ossec || ( echo "Cannot chdir to ${HOMEDIR}/ossec!" ; exit 1 )
